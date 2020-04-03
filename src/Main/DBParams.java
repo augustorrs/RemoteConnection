a3e792b6c12fd336a2c360d8e32bdf814650e5bf
Augusto Souza <augustorrsouza@gmail.com>
2020-04-03 16:54:52 +0100

    Create DBParams.java

---
+++ src/Main/DBParams.java
@@ -0,0 +1,13 @@
+package Main;
+
+/**
+ *
+ * @author Augusto Souza 2017376
+ */
+
+public class DBParams {
+        
+    public static String DATABASE_PATH = "jdbc:mysql://52.50.23.197:3306/world";
+    public static String DB_USER = "cctstudent";
+    public static String DB_PASS = "Pass1234!";
+}
