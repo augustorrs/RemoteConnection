73443b3a65bbc0a3e8de6d4b8dd2622be1b691be
Augusto Souza <augustorrsouza@gmail.com>
2020-04-03 16:59:47 +0100

    print all countries

--- src/Main/RemoteConnection.java
+++ src/Main/RemoteConnection.java
@@ -5,10 +5,33 @@
  */
 package Main;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Augusto Souza 2017376
  */
 public class RemoteConnection {
+
+/**
+     * print all countries
+     */
+    private static void printAllCountires(){
+        try{
+            List<Country> all_countries = DBCountry.readAllCountries();
+            if(all_countries == null || all_countries.isEmpty()){
+                System.out.println("Empty record !");
+                return;
+            }
+            System.out.println("Code\tName\tContinent\tSurfaceArea\tHeadOfState");
+            for(Country item : all_countries){
+                System.out.println(item.toString());
+            }
+        }catch(Exception e){
+            
+        }
+    }
     
 }
