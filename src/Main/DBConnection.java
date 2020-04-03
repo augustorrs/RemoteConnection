759742af4d17eb6e5c59f7c42089e5d355db1bf1
Augusto Souza <augustorrsouza@gmail.com>
2020-04-03 16:48:25 +0100

    variables

--- src/Main/DBConnection.java
+++ src/Main/DBConnection.java
@@ -7,5 +7,10 @@ package Main;
  */
 
 public class DBConnection {
+
+public static Connection connect = null;
+    public static Statement statement = null;
+    public static PreparedStatement preparedStatement = null;
+    public static ResultSet resultSet = null;
     
 }
