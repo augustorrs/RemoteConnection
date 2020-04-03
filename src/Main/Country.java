701ecb8073f8e09df85f5a2b20adf7f7d9428688
Augusto Souza <augustorrsouza@gmail.com>
2020-04-03 16:11:59 +0100

    public void country

--- src/Main/Country.java
+++ src/Main/Country.java
@@ -10,5 +10,24 @@ package Main;
  * @author Augusto Souza 2017376
  */
 public class Country {
+
+public Country(){
+        
+    }
+    public void setCode(String val){
+        this.code = val;
+    }
+    public void setName(String val){
+        this.name = val;
+    }
+    public void setContinent(String val){
+        this.continent = val;
+    }
+    public void setSurface(float val){
+        this.surface = val;
+    }
+    public void setHead(String val){
+        this.headOfState = val;
+    }
     
 }
