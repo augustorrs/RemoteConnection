adba02b0616e356df8e10a668f788212beb44a3a
Augusto Souza <augustorrsouza@gmail.com>
2020-04-03 16:49:10 +0100

    connect db

--- src/Main/DBConnection.java
+++ src/Main/DBConnection.java
@@ -13,4 +13,24 @@ public static Connection connect = null;
     public static PreparedStatement preparedStatement = null;
     public static ResultSet resultSet = null;
     
+/**
+     * Connect database 
+     * @return 
+     */
+    public static boolean connection() {
+        try {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connect = DriverManager.getConnection(DBParams.DATABASE_PATH, DBParams.DB_USER, DBParams.DB_PASS);
+                statement = connect.createStatement();
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
